eaf3a5ddacdc98a396246f689eecbd7ad8746fbc
elkin <elkin.21.74@gmail.com>
2025-05-04 13:31:49 -0500

    commit save progress

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -1,5 +1,7 @@
 import scala.util.Random
-import common._
+import common.*
+
+import scala.collection.parallel.immutable.ParVector
 package object Matrices {
   val random = new Random()
 
@@ -81,13 +83,14 @@ package object Matrices {
 
   def multMatrizRecPar(m1: Matriz, m2: Matriz): Matriz = {
     val n = m1.length
-    val m = n / 2
+
     n match
       //Aquí se puede definir el umbral de paralelizacion en una potencia de 2 y minimo 2
       case 2 =>
         //Una vez alcanza el umbral se utiliza la version sin paralelizar
         multMatrizRec(m1, m2)
       case _ =>
+        val m = n / 2
         val (a11, a12, a21, a22) = parallel(
           subMatriz(m1, 0, 0, m), subMatriz(m1, 0, m, m),
           subMatriz(m1, m, 0, m), subMatriz(m1, m, m, m)
@@ -100,7 +103,7 @@ package object Matrices {
           multMatrizRec(a11, b11), multMatrizRec(a12, b21),
           multMatrizRec(a11, b12), multMatrizRec(a12, b22)
         )
-        val (a21b11, a22b21, a21b12, a22b22 ) = parallel(
+        val (a21b11, a22b21, a21b12, a22b22) = parallel(
           multMatrizRec(a21, b11), multMatrizRec(a22, b21),
           multMatrizRec(a21, b12), multMatrizRec(a22, b22)
         )
@@ -127,7 +130,111 @@ package object Matrices {
     val l = m1.length
     Vector.tabulate(l,l)((i,j) => m1(i)(j) - m2(i)(j))
   }
-  
+
+  def multStrassen(m1:Matriz,m2:Matriz): Matriz = {
+    val n = m1.length
+    val m = n/2
+    n match
+      case 1 => Vector(Vector(m1(0)(0) * m2(0)(0)))
+      case _ =>
+        val A11 = subMatriz(m1, 0, 0, m); val A12 = subMatriz(m1, 0, m, m)
+        val A21 = subMatriz(m1, m, 0, m); val A22 = subMatriz(m1, m, m, m)
+        val B11 = subMatriz(m2, 0, 0, m); val B12 = subMatriz(m2, 0, m, m)
+        val B21 = subMatriz(m2, m, 0, m); val B22 = subMatriz(m2, m, m, m)
+
+        val S1 = restaMatriz(B12,B22); val S2 = sumMatriz(A11,A12)
+        val S3 = sumMatriz(A21,A22); val S4 = restaMatriz(B21,B11)
+        val S5 = sumMatriz(A11,A22); val S6 = sumMatriz(B11,B22)
+        val S7 = restaMatriz(A12,A22); val S8 = sumMatriz(B21,B22)
+        val S9 = restaMatriz(A11, A21); val S10 = sumMatriz(B11,B12)
+
+        val P1 = multStrassen(A11,S1); val P2 = multStrassen(S2,B22)
+        val P3 = multStrassen(S3,B11); val P4 = multStrassen(A22,S4)
+        val P5 = multStrassen(S5,S6); val P6 = multStrassen(S7,S8)
+        val P7 = multStrassen(S9,S10)
+
+        val C11 = sumMatriz(restaMatriz(sumMatriz(P5,P4),P2),P6)
+        val C12 = sumMatriz(P1,P2)
+        val C21 = sumMatriz(P3,P4)
+        val C22 = restaMatriz(restaMatriz(sumMatriz(P5,P1),P3),P7)
+
+
+        Vector.tabulate(n, n){ (i, j) =>
+          (i < m, j < m) match
+            case (true, true) => C11(i)(j)
+            case (true, false) => C12(i)(j - m)
+            case (false, true) => C21(i - m)(j)
+            case (false, false) => C22(i - m)(j - m)
+        }
+
+  }
+
+  def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
+    val n = m1.length
+    val m = n / 2
+    n match
+      case 1 => Vector(Vector(m1(0)(0) * m2(0)(0)))
+      case _ =>
+        val (a11, a12, a21, a22) = parallel(
+          subMatriz(m1, 0, 0, m), subMatriz(m1, 0, m, m),
+          subMatriz(m1, m, 0, m), subMatriz(m1, m, m, m)
+        )
+        val (b11, b12, b21, b22) = parallel(
+          subMatriz(m2, 0, 0, m), subMatriz(m2, 0, m, m),
+          subMatriz(m2, m, 0, m), subMatriz(m2, m, m, m)
+        )
+        val (s1, s2, s3, s4) = parallel(
+          restaMatriz(b12, b22),
+          sumMatriz(a11, a12),
+          sumMatriz(a21, a22),
+          restaMatriz(b21, b11)
+        )
+        val (s5, s6, s7, s8) = parallel(
+          sumMatriz(a11, a22),
+          sumMatriz(b11, b22),
+          restaMatriz(a12, a22),
+          sumMatriz(b21, b22)
+        )
+        val (s9,s10) = parallel(
+          restaMatriz(a11, a21),
+          sumMatriz(b11, b12)
+        )
+        val (p1, p2, p3, p4) = parallel(
+          multStrassenPar(a11, s1), 
+          multStrassenPar(s2, b22), 
+          multStrassenPar(s3, b11),
+          multStrassenPar(a22, s4)
+        )
+        val (p5,(p6, p7)) = parallel(
+          multStrassenPar(s5, s6),
+          parallel(
+            multStrassenPar(s7, s8),
+            multStrassenPar(s9, s10)
+          )
+        )
+        val (c11, c12, c21, c22) = parallel(
+          sumMatriz(restaMatriz(sumMatriz(p5, p4), p2), p6),
+          sumMatriz(p1, p2),
+          sumMatriz(p3, p4),
+          restaMatriz(restaMatriz(sumMatriz(p5, p1), p3), p7)
+        )
+        val (m1112A,m2122B) = parallel(
+          Vector.tabulate(m, n) { (i, j) =>
+            if (j < m) c11(i)(j)
+            else c12(i)(j - m)
+          },
+          Vector.tabulate(m, n) { (i, j) =>
+            if (j < m) c21(i)(j)
+            else c22(i)(j - m)
+          }
+        )
+        m1112A++m2122B
+  }
+
+  def prodPuntoParD(v1:ParVector[Int],v2:ParVector[Int]): Int = {
+    (v1 zip v2).map({ case (i, j) => i*j}).sum
+  }
+
 }
 
 
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,20 +1,98 @@
-import Matrices.*
+import Matrices._
+import Benchmark._
+//Ejecucion reducida para una ejecucion mas liviana
+for{
+  i <- 1 to 8
+  m1 = matrizAlAzar(math.pow(2,i).toInt,2)
+  m2 = matrizAlAzar(math.pow(2,i).toInt,2)
+} yield (compararAlgoritmos(multMatriz,multMatrizPar)(m1,m2),math.pow(2,i).toInt)
+for{
+  i <- 1 to 8
+  m1 = matrizAlAzar(math.pow(2,i).toInt,2)
+  m2 = matrizAlAzar(math.pow(2,i).toInt,2)
+} yield (compararAlgoritmos(multMatrizRec,multMatrizRecPar)(m1,m2),math.pow(2,i).toInt)
+for{
+  i <- 1 to 8
+  m1 = matrizAlAzar(math.pow(2,i).toInt,2)
+  m2 = matrizAlAzar(math.pow(2,i).toInt,2)
+} yield (compararAlgoritmos(multStrassen,multStrassenPar)(m1,m2),math.pow(2,i).toInt)
+compararProdPunto(100)
+/*
+//Ejecucion completa, puede tardar demasiado.
+for{
+  i <- 1 to 10
+  m1 = matrizAlAzar(math.pow(2,i).toInt,2)
+  m2 = matrizAlAzar(math.pow(2,i).toInt,2)
+} yield (compararAlgoritmos(multMatriz,multMatrizPar)(m1,m2),math.pow(2,i).toInt)
+for{
+  i <- 1 to 10
+  m1 = matrizAlAzar(math.pow(2,i).toInt,2)
+  m2 = matrizAlAzar(math.pow(2,i).toInt,2)
+} yield (compararAlgoritmos(multMatrizRec,multMatrizRecPar)(m1,m2),math.pow(2,i).toInt)
+for{
+  i <- 1 to 10
+  m1 = matrizAlAzar(math.pow(2,i).toInt,2)
+  m2 = matrizAlAzar(math.pow(2,i).toInt,2)
+} yield (compararAlgoritmos(multStrassen,multStrassenPar)(m1,m2),math.pow(2,i).toInt)
+*/
 
 
-val matriz = matrizAlAzar(math.pow(2,5).toInt,2)
-val matriz2 = matrizAlAzar(math.pow(2,5).toInt,2)
+val m1A = matrizAlAzar(math.pow(2,1).toInt,2)
+val m2A = matrizAlAzar(math.pow(2,2).toInt,2)
+val m3A = matrizAlAzar(math.pow(2,6).toInt,2)
+val m4A = matrizAlAzar(math.pow(2,4).toInt,2)
+val m5A = matrizAlAzar(math.pow(2,7).toInt,2)
 
-val matriz1 = Vector(Vector(1,2),Vector(1,0))
-val matriz11 = Vector(Vector(1,0),Vector(1,1))
+val m1B = matrizAlAzar(math.pow(2,1).toInt,2)
+val m2B = matrizAlAzar(math.pow(2,2).toInt,2)
+val m3B = matrizAlAzar(math.pow(2,6).toInt,2)
+val m4B = matrizAlAzar(math.pow(2,4).toInt,2)
+val m5B = matrizAlAzar(math.pow(2,7).toInt,2)
+
+val V1A = vectorAlAzar(1,2)
+val V2A = vectorAlAzar(2,2)
+val V3A = vectorAlAzar(3,2)
+val V4A = vectorAlAzar(5,2)
+val V5A = vectorAlAzar(6,2)
+
+multMatriz(m1A,m1B)
+multMatriz(m2A,m2B)
+multMatriz(m3A,m3B)
+multMatriz(m4A,m4B)
+multMatriz(m5A,m5B)
+
+multMatrizPar(m1A,m1B)
+multMatrizPar(m2A,m2B)
+multMatrizPar(m3A,m3B)
+multMatrizPar(m4A,m4B)
+multMatrizPar(m5A,m5B)
+
+multMatrizRec(m1A,m1B)
+multMatrizRec(m2A,m2B)
+multMatrizRec(m3A,m3B)
+multMatrizRec(m4A,m4B)
+multMatrizRec(m5A,m5B)
+
+multMatrizRecPar(m1A,m1B)
+multMatrizRecPar(m2A,m2B)
+multMatrizRecPar(m3A,m3B)
+multMatrizRecPar(m4A,m4B)
+multMatrizRecPar(m5A,m5B)
+
+multStrassen(m1A,m1B)
+multStrassen(m2A,m2B)
+multStrassen(m3A,m3B)
+multStrassen(m4A,m4B)
+multStrassen(m5A,m5B)
+
+multStrassenPar(m1A,m1B)
+multStrassenPar(m2A,m2B)
+multStrassenPar(m3A,m3B)
+multStrassenPar(m4A,m4B)
+multStrassenPar(m5A,m5B)
 
 
 
-multMatriz(matriz, matriz2)
-multMatrizPar(matriz,matriz2)
-multMatrizRec(matriz,matriz2)
-multMatrizRecPar(matriz,matriz2)
-
-restaMatriz(matriz1,matriz11)
 
 
 
